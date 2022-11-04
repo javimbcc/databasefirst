875adc04b76d5ce540b765cbb3aab9b95854aa40
DESKTOP-HGE6DGN\javi <csi22-jmennza@colegioaltair.net>
2022-11-04 09:06:51 +0100

    [9:06 3/11/2022]: Añadido apartado pruebaVistas con su respectivo controlador y enlace en el navbar

---
+++ DatabaseFirst/Controllers/PruebaVistaControlador.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseFirst.Controllers
+{
+    public class PruebaVistaControlador : Controller
+    {
+        // GET: PruebaVistaControlador
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PruebaVistaControlador/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PruebaVistaControlador/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PruebaVistaControlador/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PruebaVistaControlador/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PruebaVistaControlador/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PruebaVistaControlador/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PruebaVistaControlador/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
